d21a0b1d796a681222a1ea8dfa289a51af98e580
yakun.shen <2622968820@qq.com>
2019-04-12 20:02:07 +0800

    纯净的springboot框架

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+    @RequestMapping("/")
+    @ResponseBody
+    public String test() {
+        return "Hello World 1234";
+    }
+}
